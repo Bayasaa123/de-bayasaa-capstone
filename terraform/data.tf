38aba1c270afcd7fb467430a9739ebde704ac996
Bayasaa123 <bayarbayasgalan.888@gmail.com>
2025-09-21 20:47:05 +1000

    fixing issue

--- terraform/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_secretsmanager_secret" "yt" {
-    name = "youtube_secret"
-}
-
-data "aws_secretsmanager_secret_version" "yt_latest" {
-    secret_id = data.aws_secretsmanager_secret.yt.id
-}

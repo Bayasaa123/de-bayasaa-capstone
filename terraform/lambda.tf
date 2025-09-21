a1a1a022190abb6162ee39187739d872fde38cc9
Bayasaa123 <bayarbayasgalan.888@gmail.com>
2025-09-21 20:45:34 +1000

    fixing issue

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -46,13 +46,13 @@ resource "aws_lambda_function" "api_reader" {
 
   environment {
     variables = {
-        MAX_VIDEOS     = "5"
-        MAX_COMMENTS   = "10"
-        YT_QUERY       = "data engineering"
-        SECRET_NAME = local.yt_api_key
-        OUTPUT_BUCKET = module.data_bucket.bucket_name
-      }
+      MAX_VIDEOS     = "5"
+      MAX_COMMENTS   = "10"
+      YT_QUERY       = "data engineering"
+      SECRET_NAME = local.yt_api_key
+      OUTPUT_BUCKET = module.data_bucket.bucket_name
     }
+  }
   }
 
 
